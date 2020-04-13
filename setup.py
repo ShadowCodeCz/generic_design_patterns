184f910ff25711fc03e458cb66bf18346c900995
Honzin <xsedaj00@gmail.com>
2020-04-13 17:54:37 +0200

    v1.0.9

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ description = "Python package implements design patterns in generic way. Its can
 
 setup(
     name='generic_design_patterns',
-    version='1.0.8',
+    version='1.0.9',
     packages=['generic_design_patterns'],
     url='https://github.com/ShadowCodeCz/generic_design_patterns',
     project_urls={
